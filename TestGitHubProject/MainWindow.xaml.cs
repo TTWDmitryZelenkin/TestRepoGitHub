9ee0c6c3f657992a6e011f81e310665f49a9662e
Dmitry <dmitry.zelenkin@ttw.com.au>
2018-03-06 11:51:37 +1100

    New branch first commit

--- TestGitHubProject/MainWindow.xaml.cs
+++ TestGitHubProject/MainWindow.xaml.cs
@@ -24,6 +24,6 @@ namespace TestGitHubProject
         {
             InitializeComponent();
         }
-        // this is a test one. Second commit. Initial branch
+        // this is a test one. Second commit. Bindings Branch
     }
 }
